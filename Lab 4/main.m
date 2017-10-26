0d57dc7135c15ead5ef02d0695505be872b3e0ac
Francescodario Cuzzocrea <bosconovic@gmail.com>
2017-10-26 09:48:38 +0200

    Update on Lab 4

--- Lab 4/main.m
+++ Lab 4/main.m
@@ -21,10 +21,14 @@ I_3 = 0.0700;
 
 %% INITIAL CONDITIONS
 
-% Dynamics
-w1_0 = 0;
-w2_0 = 0;
-w3_0 = sqrt((6.674*10^-11*(5.972*10^24))/ (200000+6378000)^3);
+%Dynamics
+% w1_0 = 0;
+% w2_0 = 0;
+% w3_0 = sqrt((6.674*10^-11*(5.972*10^24))/ (200000+6378000)^3);  
+
+w1_0 = 0.56;
+w2_0 = 0.64;
+w3_0 = 0.587;  
 
 % Kinematics : DCM
 DCM_0 = eye(3);
@@ -35,8 +39,8 @@ theta_0 = 0.1;
 psi_0 = 0.1;
 
 % Pointing orbit
-mu = 398600;  %km
-a = 200+6378;
+mu = 398600;    %km
+a = 200+6378;   %km
 e = 0;
 i = 0;
 OMG = 0;
@@ -50,10 +54,20 @@ sim Lab_4
 
 %% OUTPUTS PLOT
 
-% Pointing vectaarr
+% Pointing vector
 figure(1)
 plot3(X_P(1,:),X_P(2,:),X_P(3,:));
+axis vis3d 
 
 % Reference orbit
 figure(2)
 plot3(r(:,1),r(:,2),r(:,3))
+axis vis3d
+        
+%{
+   TODO : 
+           * Quaternions
+           * Gravity Gradient Torque
+%}
+
+          
