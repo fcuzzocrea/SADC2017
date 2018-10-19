7ee393054d7e080b6d63e18455568c8c6d028925
Francescodario Cuzzocrea <bosconovic@gmail.com>
2018-10-19 13:10:24 +0200

    Lab 3: fix the computation of the Earth Pointing LVLH frame to track
    We compute S/C dynamics using Euler's Equations, which gives us the attitude in the body frame which has the x axis pointing toward the space, the y axis pointing toward the direction of motion and the z axis pointing in the direction of the angular momentum. So attitude matrix A_BN (body/inertial) is given in the test case by :
    
            1 0 0
    A_BN =  0 1 0
            0 0 1
    
    Then we compute the tracking LVLH frame to track a generic point on the earth, this reference frame has the x axis (o_1) pointing towards the Earth (so the Nadir direction), the y axis (o_2) pointing toward the direction of motion and the z axis (o_3) pointing in the opposite direction WRT the angular momentum of the S/C :
    
    o_1 = -r/norm(r);                                       % Points toward the Earth, aligned with S/C - Earth direction (x)
    o_3 = -(cross(r,r_dot))/(norm(cross(r,r_dot)));         % Points in the direction of angular momentum vector (z)
    o_2 = cross(o_3,o_1);                                   % Points into the direction of the motion (y)
    
    So the matrix associated with this frame is given by :
    
            -1  0  0
    A_LN =   0  1  0
             0  0 -1
    
    So, considering a test case, were w1 = 0, w2 = 0 and w3 = mean motion of the reference equatorial circular orbit, whitout any control the error between the two frames is constant and is given by :
    
    A_BL = A_BN*A_LN' =
    
                         -1  0  0
    A_BL = A_BN*A_LN' =   0  1  0
                          0  0 -1
    
    Signed-off by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- Lab 3/main.m
+++ Lab 3/main.m
@@ -11,49 +11,17 @@ clear
 clc
 
 %% GEOMETRICAL PROPERTIES
-% Datas taken from ISIS space : https://www.isispace.nl/product/6-unit-cubesat-structure/
-%
-%
-%                                        ^
-%                                        |
-%                                        |   z
-%
-%                             +--------------------------+
-%                            /                          /|
-%                           /                          / |
-%                          /                          /  |
-%                         +--------------------------+   |  ---> y
-%                         |                          |   +
-%                         | w                        | h/
-%                         |           l              | /
-%                         +--------------------------+
-%                                     /
-%                                    /  x
-%                                  \/
-% Mass of 6U cubesat + payload
-M = 3.00;         % Kg
 
-% Dimensions
-w = 100*10^-3;    % m
-l = 226.3*10^-3;  % m
-h = 340.5*10^-3;  % m
-dim = [w, l, h];
+% Triaxial symmetry case, J2 intermediate principal moment of inertia
+I_1 = 0.0109;                % Kgm^2
+I_2 = 0.0504;                % Kgm^2
+I_3 = 0.055;                 % Kgm^2    
 
-%Sections
-A_i_1 = w*l; % m^2
-A_i_2 = w*h; % m^2
-A_i_3 = l*h; % m^2
-A_i = [A_i_1, A_i_2, A_i_3];
-
-I_1 = M/12 * (w^2 + l^2); % Kg * m^2
-I_2 = M/12 * (w^2 + h^2); % Kg * m^2
-I_3 = M/12 * (l^2 + h^2); % Kg * m^2
-
-I = [I_1  0   0;
-      0  I_2  0;
-      0   0  I_3];
-  
-I_inv = inv(I);  
+I = [I_1,   0,  0;
+        0,I_2,  0;
+        0,  0, I_3];   
+    
+I_inv = inv(I);    
 %% INITIAL CONDITIONS
 
 % Standard gravitational parameter
@@ -63,21 +31,21 @@ mu = 398600;        % Km^3/s^2
 R_e = 6.378e+03;    % Km
 
 % Test omegas
-w1_0 = 0.012;
-w2_0 = 0.09;
-w3_0 = 0.17;
+w1_0 = 0;
+w2_0 = 0;
+w3_0 = 0.001038158026828;
 
 w_0 = [w1_0,w2_0,w3_0];
 
 % Kinematics : DCM
-DCM_0 = eye(3);
+A_BN_0 = eye(3);
 
 % Kinematics : Euler Angles
-phi_0 = 0.1;
-theta_0 = 0.1;
-psi_0 = 0.1;
+phi_0 = 0;
+theta_0 = 0;
+psi_0 = 0;
 
-% Pointing orbit
+% LVLH Orbit Setup
 r_a = 800 + R_e;
 r_p = 800 + R_e;                % Km
 a = (r_a + r_p)/2;              % Km
@@ -98,6 +66,6 @@ sim Lab_3
 figure(1)
 plot3(X_P(1,:),X_P(2,:),X_P(3,:));
 
-% Pointing orbit
+% LVLH orbit
 figure(2)
-plot3(r(:,1),r(:,2),r(:,3))
+plot3(r_LVLH(:,1),r_LVLH(:,2),r_LVLH(:,3))
