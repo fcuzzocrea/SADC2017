52e2c1942f998b88a6e0176e8644ececa5ebdc0f
Francescodario Cuzzocrea <bosconovic@gmail.com>
2018-10-26 09:26:04 +0200

    Lab 4: refactor gravity gradient blocks to perform a vectoria computation of the torque due to the gravity gradient and refactor the control law.
    The gravity gradient is now computed as :
    
    L_gg = 3*mu/norm(r)^3*n x (I*n)
    
    where n is the nadir pointing unit vector, wich points toward the nadir. In our case the nadir direction is aligned with the x axis so n is computed as :
    
    n = A_BL * [1 0 0]'
    
    The control law implemented outputs a torque given by
    
    L = -k_p*sign(q_e_4)*[q_e_13] - k_d * w_bn
    
    Where q_e is the error quaternion given by the quaternion product
    
    q_e = q * q_c^-1
    
    q_c is the command quaternion chosen as q_c = [0; 1; 0; 0], so it tracks the LVLH frame represented by :
    
            -1  0  0
    A_LN =   0  1  0
             0  0 -1

--- Lab 4/main.m
+++ Lab 4/main.m
@@ -10,52 +10,75 @@ close all
 clear 
 clc
 
-mu = 398600;
-R_e = 6378;       % km
+%% GEOMETRICAL PROPERTIES
 
+% Triaxial symmetry case, J2 intermediate principal moment of inertia
+I_1 = 0.0109;                % Kgm^2
+I_2 = 0.0504;                % Kgm^2
+I_3 = 0.055;                 % Kgm^2    
 
-%% INERTIAL PROPERTIES
-
-I_1 = 0.0134;
-I_2 = 0.0666;
-I_3 = 0.0666;
-
+I = [I_1,   0,  0;
+        0,I_2,  0;
+        0,  0, I_3];   
+    
+I_inv = inv(I);    
 %% INITIAL CONDITIONS
 
+% Standard gravitational parameter
+mu = 398600;        % Km^3/s^2
+
+% Radius of the Earth 
+R_e = 6.378e+03;    % Km
+
 % Test omegas
-%
-w1_0 = 0;
-w2_0 = 0;
-%w3_0 = 0.54;
-w3_0 = sqrt(mu/(R_e+800)^3);
+w1_0 = 0.02;
+w2_0 = 0.01;
+w3_0 = 0.05;
+%w3_0 = 0.001038158026828;  % Mean motion of the orbit
+w_0 = [w1_0,w2_0,w3_0];
 
-% Kinematics : DCM
-DCM_0 = eye(3);
 
+% Initial Attitude
+A_BN_0 = eye(3);
+    
 % Kinematics : Euler Angles
-phi_0 = 0.1;
-theta_0 = 0.1;
-psi_0 = 0.1;
+phi_0 = 0;
+theta_0 = 0;
+psi_0 = 0;
 
-q_0 = [0 1/sqrt(3) 1/sqrt(3) 1/sqrt(3)];
+% LVLH Orbit Setup
+r_a = 800 + R_e;
+r_p = 800 + R_e;                % Km
+a = (r_a + r_p)/2;              % Km
+e = (r_a - r_p)/(r_a + r_p);
+i = 0;                          % Degrees
+OMG = 0;                        % Degrees
+omg = 0;                        % Degrees
+theta = 0;                      % Degrees
 
-% Pointing orbit (test)
-a = 800+R_e;   %km
-e = 0.1;
-i = 0;
-OMG = 0;
-omg = 0;
-theta = 0;
+%% DISTURBANCES
 
-%% CONTROL 
+% Gravity Gradient
+nadir_vector = [1; 0; 0];
 
-% We want to be aligned with the LVLH frame, so
+%% CONTROLLER SETUP
 
-q_c = [0 0 0 1]';
+% Gains
+k_p = -0.001;
+k_d = -0.001;
 
+% Commanded quaternion 
+q_c = [0; 1; 0; 0];
+
+Q_C = [ q_c(4)  q_c(3)  -q_c(2) -q_c(1) ;
+       -q_c(3)  q_c(4)   q_c(1) -q_c(2) ;
+        q_c(2) -q_c(1)   q_c(4) -q_c(3) ; 
+        q_c(1)  q_c(2)   q_c(3)  q_c(4) ];
+    
 %% LAUNCH SIMULATOR
 
-simulation_time = (2*pi*sqrt(a^3/mu));
+% Set simulation time
+simulation_time = (2*pi*sqrt(a^3/mu))*3;
 sim Lab_4    
 
 %% OUTPUTS PLOT
@@ -63,12 +86,10 @@ sim Lab_4
 % Pointing vector
 figure(1)
 plot3(X_P(1,:),X_P(2,:),X_P(3,:));
-axis equal 
 
-% Reference orbit
+% LVLH orbit
 figure(2)
-plot3(r(:,1),r(:,2),r(:,3))
-axis equal
+plot3(r_LVLH(:,1),r_LVLH(:,2),r_LVLH(:,3));
 
 % Gravity Gradient
 figure(3)
@@ -76,13 +97,3 @@ hold on
 plot(GG(1:end,1)); plot(GG(1:end,2)); plot(GG(1:end,3));
 title('Disturbance Torque due to Gravity Gradient')
 legend('GG_x','GG_y','GG_z')
-
-% Error on attitude
-figure(4)
-hold on
-plot(w_bl(1:700,1)); plot(w_bl(1:700,2)); plot(w_bl(1:700,3));
-title('Error between BFF and RF')
-legend('x','y','z')
-    
-
-          
