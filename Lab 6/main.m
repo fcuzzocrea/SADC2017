4bad9affb86dcb372bebef69f834ad2300281bff
Francescodario Cuzzocrea <bosconovic@gmail.com>
2018-10-31 12:14:41 +0100

    Lab 6: import updates from Lab 5

--- Lab 6/main.m
+++ Lab 6/main.m
@@ -31,75 +31,82 @@ clc
 %                                    /  x
 %                                  \/
 %
+
 % Mass of 6U cubesat + payload
-M = 3.00;         % Kg
+M = 3.00;                           % Kg
 
 % Dimensions
-w = 100*10^-3;    % m
-l = 226.3*10^-3;  % m
-h = 340.5*10^-3;  % m
+w = 100*10^-3;                      % m
+l = 226.3*10^-3;                    % m
+h = 340.5*10^-3;                    % m
 dim = [w, l, h];
 
 %Sections
-A_i_1 = w*l; % m^2
-A_i_2 = w*h; % m^2
-A_i_3 = l*h; % m^2
+A_i_1 = w*l;                        % m^2
+A_i_2 = w*h;                        % m^2
+A_i_3 = l*h;                        % m^2
 A_i = [A_i_1, A_i_2, A_i_3];
 
-I_1 = M/12 * (w^2 + l^2); % Kg * m^2
-I_2 = M/12 * (w^2 + h^2); % Kg * m^2
-I_3 = M/12 * (l^2 + h^2); % Kg * m^2
+I_1 = M/12 * (w^2 + l^2);           % Kg * m^2
+I_2 = M/12 * (w^2 + h^2);           % Kg * m^2
+I_3 = M/12 * (l^2 + h^2);           % Kg * m^2
+
+I = [I_1,   0,  0;
+        0,I_2,  0;
+        0,  0, I_3];   
+    
+I_inv = inv(I);
 
 %% INITIAL CONDITIONS
 
 % Standard gravitational parameter
-mu = 398600;        % Km^3/s^2
+mu = 398600;                        % Km^3/s^2
 
 % Radius of the Earth 
-R_e = 6.378e+03;    % Km
+R_e = 6.378e+03;                    % Km
 
 % Test omegas
-w1_0 = 0.1;
-w2_0 = 0.2;
-w3_0 = 0.5;
-%w3_0 = (sqrt(mu/(7.9755e+03)^3));  % rad/s
-%w3_0 = (sqrt(mu/(7.9755e+03)^3))*100;  % rad/s
-%w3_0 = 0.0872665;
-%w3_0 = 0.0080;
+w1_0 = 0.02;                        % rad/s
+w2_0 = 0.01;                        % rad/s
+w3_0 = 0.05;                        % rad/s
+%w3_0 = 0.001038158026828;  % Mean motion of the orbit
+w_0 = [w1_0,w2_0,w3_0];
 
-% Kinematics : DCM
-DCM_0 = eye(3);
 
-% Kinematics : Euler Angles
-phi_0 = 0.1;
-theta_0 = 0.1;
-psi_0 = 0.1;
-q_0 = [0 1/sqrt(3) 1/sqrt(3) 1/sqrt(3)];
+% Initial Attitude
+A_BN_0 = eye(3);
 
-% Pointing orbit (test)
-%r_a = 2395 + R_e;               % Km
-r_a = 800 + R_e;
-r_p = 800 + R_e;                % Km
-a = (r_a + r_p)/2;              % Km
+% Euler Angles
+phi_0 = 0;                          % Degrees
+theta_0 = 0;                        % Degrees
+psi_0 = 0;                          % Degrees
+
+% LVLH Orbit 
+r_a = 800 + R_e;                    % Km
+r_p = 800 + R_e;                    % Km
+a = (r_a + r_p)/2;                  % Km
 e = (r_a - r_p)/(r_a + r_p);
-i = 0;                          % Degrees
-OMG = 0;                        % Degrees
-omg = 0;                        % Degrees
-theta = 0;                      % Degrees
+i = 40;                             % Degrees
+OMG = 0;                            % Degrees
+omg = 0;                            % Degrees
+theta = 0;                          % Degrees
 
 %% CONDITIONS FOR DISTURBANCES
 
+% Gravity Gradient
+nadir_vector = [1; 0; 0];
+
 % Angular velocity of the Earth
-w_e = [0 0 0.00007291];  % rad/sec
+w_e = [0 0 0.00007291];             % rad/s
 
 % Sun Mean Motion
-n = 2*pi/(60*60*24*365); % rad/s   
+n = 2*pi/(60*60*24*365);            % rad/s   
 
-% Obliquity of the Ecliptic
-eps = deg2rad(23.439);   % rad       
+% Obliquity of the ecliptic plane
+epsilon = deg2rad(23.439);          % rad       
 
 % Air properties
-rho = 1.31e-17*1000;     % Kg/m^3 @ 800km
+rho = 1.31e-17*1000;                % Kg/m^3 @ 800km
 C_d = 2.2;
 
 % Reflection cofficients
@@ -110,45 +117,52 @@ rho_d = 0.1;
 alpha_m = deg2rad(11);
 
 % Earth's magnetic radius 
-a_m = 6371.2;         % m
+a_m = 6371.2;                       % m
 
 % IGRF 2005 Constants
-g_1_0 = -29554*10^-9;      % T
-g_1_1 = -1669.05*10^-9;    % T
-h_1_1 = 5077.99*10^-9;     % T
+g_1_0 = -29554*10^-9;               % T
+g_1_1 = -1669.05*10^-9;             % T
+h_1_1 = 5077.99*10^-9;              % T
 
-H_0 = sqrt((g_1_0)^2 + (g_1_1)^2 + (h_1_1)^2);  % T
+H_0 = sqrt((g_1_0)^2 +  ...
+     (g_1_1)^2 + (h_1_1)^2);        % T
 
 % Vacuum permeability, to convert m^3*T in A*m^2
-mu_0 = 4*pi*1e-7;            % T*m/A
+mu_0 = 4*pi*1e-7;                   % T*m/A
 
 % Magnitude of the dipole
-m_m = (a_m^3*H_0)*(4*pi/mu_0);  % Am^2
+m_m = (a_m^3*H_0)*(4*pi/mu_0);      % Am^2
 
-%% CONTROL 
+%% CONTROLLER SETUP
 
-% We want to be aligned with the LVLH frame, so
+% Gains
+k_p = -0.001;
+k_d = -0.001;
+%k_d = 0; k_p = k_d;
 
-q_c = [0 0 0 1]';
+% Commanded quaternion 
+%q_c = [0; 1; 0; 0];
+q_c = [0; 0.4081; 0.9129; 0];    
+
+Q_C = [ q_c(4)  q_c(3)  -q_c(2) -q_c(1) ;
+       -q_c(3)  q_c(4)   q_c(1) -q_c(2) ;
+        q_c(2) -q_c(1)   q_c(4) -q_c(3) ; 
+        q_c(1)  q_c(2)   q_c(3)  q_c(4) ];
 
 %% LAUNCH SIMULATOR
 
 simulation_time = (2*pi*sqrt(a^3/mu));
-sim Lab_6    
-
-OMEGA_ORB =    8.8640e-04;  % Per ricavare il gain, ma tantonon lo uso piu
+sim Lab_5    
 
 %% OUTPUTS PLOT
 
 % Pointing vector
 figure(1)
-plot3(X_P(1,:),X_P(2,:),X_P(3,:));
-axis equal 
+plot3(X_P(1,:),X_P(2,:),X_P(3,:)); 
 
 % Reference orbit
 figure(2)
-plot3(r(:,1),r(:,2),r(:,3))
-axis equal
+plot3(r_LVLH(:,1),r_LVLH(:,2),r_LVLH(:,3))
 
 % Gravity Gradient
 figure(3)
@@ -183,4 +197,4 @@ figure(7)
 hold on
 plot(w_bl(1:end,1)); plot(w_bl(1:end,2)); plot(w_bl(1:end,3));
 title('Error between BFF and RF')
-legend('x','y','z')      
+legend('x','y','z')
